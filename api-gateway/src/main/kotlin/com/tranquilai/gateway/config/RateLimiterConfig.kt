3f8a1bfcc60b6771f98213a3b08ac4862b4f5e1a
Zacle <zacharienziuki@gmail.com>
2026-05-30 08:01:44 +0300

    Add primary annotation to remoteAddrKeyResolver bean in RateLimiterConfig

--- api-gateway/src/main/kotlin/com/tranquilai/gateway/config/RateLimiterConfig.kt
+++ api-gateway/src/main/kotlin/com/tranquilai/gateway/config/RateLimiterConfig.kt
@@ -3,12 +3,14 @@ package com.tranquilai.gateway.config
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import reactor.core.publisher.Mono
 
 @Configuration
 class RateLimiterConfig {
 
     @Bean
+    @Primary
     fun remoteAddrKeyResolver(): KeyResolver = KeyResolver { exchange ->
         Mono.justOrEmpty(exchange.request.remoteAddress?.address?.hostAddress)
             .defaultIfEmpty("unknown")
